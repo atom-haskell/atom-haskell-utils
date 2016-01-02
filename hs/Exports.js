e78e27decc20e22ec91ed78d7703a6908ae04eff
Nikolay Yakimov <root@livid.pp.ru>
2016-01-02 22:27:19 +0300

    Remove 'main' from Export.js

--- hs/Exports.js
+++ hs/Exports.js
@@ -1,7 +1,3 @@
-global["main"] = function() {
-  h$run(h$mainZCMainzimain);
-}
-
 global["parseDotCabal"] = function(input, callback) {
   var action = h$c3( h$ap2_e
                    , h$mainZCMainziparseDotCabal
